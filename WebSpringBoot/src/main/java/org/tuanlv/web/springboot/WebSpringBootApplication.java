6a02d0161f65681b4de51e6bb0fec978e1f730af
levtuanit <levtuanit@gmail.com>
2018-12-19 10:09:25 +0700

    Create new WebSpringBoot Project
    Create new WebSpringBoot Project

---
+++ WebSpringBoot/src/main/java/org/tuanlv/web/springboot/WebSpringBootApplication.java
@@ -0,0 +1,14 @@
+package org.tuanlv.web.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSpringBootApplication.class, args);
+	}
+
+}
+
